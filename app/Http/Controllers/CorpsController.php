6a1e96a2684ebaefa61c28198933d0f982c0aed2
SiranJohn <zsrxxh@163.com>
2018-04-30 23:58:35 +0800

    实际上一下一户

--- app/Http/Controllers/CorpsController.php
+++ app/Http/Controllers/CorpsController.php
@@ -14,18 +14,20 @@ class CorpsController extends Controller
      */
     public function index(Request $request)
     {
-
-        $div = $request->div;
-        $type = $request->identifier;
-
-        $request->session()->put('div', $div);
+        $type = $request->type ?? session('type');
+        $div = $request->div ?? session('div');
+        $page = $request->page ?? session('page') ?? '';
+        
         $request->session()->put('type', $type);
+        $request->session()->put('div', $div);
+        $request->session()->put('page', $page);
 
         $corps = Corp::where('type', $type)
                 ->where('Division', $div)
-                // ->get()
                 ->paginate(20);
-        return view('corp.index',['corps' => $corps]);
+
+        $data = ['corps' => $corps, 'div'=>$div, 'type'=>$type, 'page'=>$page];
+        return view('corp.index', $data);
     }
 
     /**
@@ -38,7 +40,12 @@ class CorpsController extends Controller
     {
         $corp = Corp::find($id); //find得到的是object,可以在BLADE中使用$corp->xxx
             // ->get(); get得到的是collection,需要使用$corp[0]->xxx,或者 $corp->first()->xxx
-        return view('corp.show', ['corp'=>$corp]);        
+
+        $type = session('type') ?? $corp->type;
+        $div = session('div') ?? $corp->div;
+        $page = session('page') ?? '';
+        $data = ['corp' => $corp, 'div'=>$div, 'type'=>$type, 'page'=>$page];
+        return view('corp.show', $data);
     }
     
     /**
@@ -51,7 +58,12 @@ class CorpsController extends Controller
     {
         $corp = Corp::find($id); //find得到的是object,可以在BLADE中使用$corp->xxx
             // ->get(); get得到的是collection,需要使用$corp[0]->xxx,或者 $corp->first()->xxx
-        return view('corp.edit', ['corp'=>$corp]);
+        $type = session('type') ?? $corp->type;
+        $div = session('div') ?? $corp->div;
+        $page = session('page') ?? '';
+        $data = ['corp' => $corp, 'div'=>$div, 'type'=>$type, 'page'=>$page];
+
+        return view('corp.edit', $data);
     }
 
     /**
@@ -75,5 +87,61 @@ class CorpsController extends Controller
             ]);
         }
         // return redirect(route('corp.edit', $id));
+    }
+
+    public function next(Request $request, $id)
+    {
+        $corp = Corp::find($id);
+        $division = $corp->Division;
+        $type = $corp->type;
+        $div_corp_index = $corp->div_corp_index;
+
+        $list_with_type_div = Corp::where('Division', $division)
+            ->where('type', $type)
+            ->orderBy('div_corp_index')
+            ->get();
+
+        if ($corp->div_corp_index === $list_with_type_div->count()) {
+            // 最后一户
+            $next_corp = Corp::where('Division', $division)
+                ->where('type', $type)
+                ->where('div_corp_index', 1)
+                ->get();
+        } else {
+            $next_corp = Corp::where('Division', $division)
+                ->where('type', $type)
+                ->where('div_corp_index', $div_corp_index + 1)
+                ->get();
+        }
+
+        return redirect(route('corp.edit', $next_corp[0]->RegNum));
+    }
+
+    public function prev(Request $request, $id)
+    {
+        $corp = Corp::find($id);
+        $division = $corp->Division;
+        $type = $corp->type;
+        $div_corp_index = $corp->div_corp_index;
+
+        $list_with_type_div = Corp::where('Division', $division)
+            ->where('type', $type)
+            ->orderBy('div_corp_index')
+            ->get();
+
+        if ($corp->div_corp_index === 1) {
+            // 最后一户
+            $prev_corp = Corp::where('Division', $division)
+                ->where('type', $type)
+                ->where('div_corp_index', $list_with_type_div->count())
+                ->get();
+        } else {
+            $prev_corp = Corp::where('Division', $division)
+                ->where('type', $type)
+                ->where('div_corp_index', $div_corp_index - 1)
+                ->get();
+        }
+
+        return redirect(route('corp.edit', $prev_corp[0]->RegNum));
     }
 }
